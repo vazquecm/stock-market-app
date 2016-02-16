17ea90929f33e5432c1262288396f06692bba4b9
Carmen Vazquez-Marin <vazquecm@gmail.com>
2016-02-16 13:50:08 -0600

    installed & set up path

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const app = express();
 
 // ./routes/ automatically looks for index if you don't specify a different one
 const routes = require('./routes/');
+const path = require('path');
+
 
 // set port to environment port or 3000
 const PORT = process.env.PORT || 3000;
@@ -16,7 +18,8 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', 'jade');
 
 app.use(routes);
-
+// express.static = I'm getting html, css files already made/not changing
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // setting up our server
